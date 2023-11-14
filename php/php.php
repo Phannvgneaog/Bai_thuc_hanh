39002bc39f74c6e29677f40ac930a822f0a925ef
Phannvgneaog <tuandong600@gmail.com>
2023-11-14 15:21:25 +0700

    loop php

--- php/php.php
+++ php/php.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-<meta charset="utf-8">
-<title></title>
-</head>
-<body>
-Result:
 <?php
-/* php code starts from here */
-$result = $_GET["firstNumber"] + $_GET["secondNumber"]; // calculate the result
-echo $result; // print result to web screen
+for ($x = 1; $x <= 10; $x++) 
+{
+  print("Day la so $x <br>");
+}
 ?>
-</body>
-</html>
