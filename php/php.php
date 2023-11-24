471cd71224deb83b120e43600885e5ed71d2adef
Phannvgneaog <tuandong600@gmail.com>
2023-11-24 09:18:52 +0700

    if ..else

--- php/php.php
+++ php/php.php
@@ -1,21 +1,17 @@
-
 <?php
+  // before if statement
+  echo "THE GAME ";
 
+  // if statement; condition: $_GET["response"] == "yes"
+  if ($_GET["response"] == "yes") {
+    echo "HAS BEEN QUITED";
+  }
 
-before if statement
-echo "THE GAME ";
+  // if statement; condition: $_GET["response"] == "no"
+  else {
+    echo "WILL BE CONTINUED IN 3 SECONDS";
+  }
 
-if statement; condition: $_GET["response"] == "yes"
-if ($_GET["response"] == "yes") {
- echo "HAS BEEN QUITED";
-}
-
-// if statement; condition: $_GET["response"] == "no"
-if ($_GET["response"] == "no") {
- echo "WILL BE CONTINUED IN 3 SECONDS";
-}
-
-// after if statement
-echo "<br>AFTER IF STATEMENT";
+  // after if statement
+  echo "<br>AFTER IF STATEMENT";
 ?>
-
