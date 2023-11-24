122384670fe1f2699c1ad046d3b8f8a2df59fa21
Phannvgneaog <tuandong600@gmail.com>
2023-11-24 09:25:11 +0700

    array

--- php/php.php
+++ php/php.php
@@ -1,47 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Chuyển đổi ngoại tệ</title>
-</head>
-<body>
-    <h2>Chuyển đổi ngoại tệ</h2>
+<?php
 
-    <form method="post" action="">
-        <label for="amount">Số tiền:</label>
-        <input type="text" id="amount" name="amount" required><br><br>
+$exchangeRate = array(
+  "USD" => 22,360,
+  "EUR" => 27,368,
+  "SGD" => 17,689,
+  "JPY" => 120
+);
 
-        <label for="from_currency">Từ:</label>
-        <select id="from_currency" name="from_currency">
-            <option value="USD">USD</option>
-            <option value="EUR">EUR</option>
-            <option value="GBP">GBP</option>
-            <!-- Thêm các loại tiền tệ khác nếu cần -->
-        </select><br><br>
+echo "<h3>Exchange rate to VND</h3>";
+echo "USD: $exchangeRate[0]<br>";
+echo "EUR: $exchangeRate[1]<br>";
+echo "SGD: $exchangeRate[2]<br>";
+echo "JPY: $exchangeRate [3]<br>";
 
-        <label for="to_currency">Sang:</label>
-        <select id="to_currency" name="to_currency">
-            <option value="USD">USD</option>
-            <option value="EUR">EUR</option>
-            <option value="GBP">GBP</option>
-            <!-- Thêm các loại tiền tệ khác nếu cần -->
-        </select><br><br>
-
-        <input type="submit" name="convert" value="Chuyển đổi">
-    </form>
-
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["convert"])) {
-        $amount = $_POST["amount"];
-        $from_currency = $_POST["from_currency"];
-        $to_currency = $_POST["to_currency"];
-
-        // Thực hiện chuyển đổi ngoại tệ ở đây (có thể sử dụng API hoặc một tỷ giá cố định)
-        // Đây chỉ là một ví dụ đơn giản, bạn cần thay đổi để chuyển đổi thực tế
-        $conversion_rate = 1.2; // Tỷ giá từ USD sang EUR
-
-        $result = $amount * $conversion_rate;
-        echo "<br>Kết quả: " . $amount . " " . $from_currency . " = " . $result . " " . $to_currency;
-    }
-    ?>
-</body>
-</html>
+?>
