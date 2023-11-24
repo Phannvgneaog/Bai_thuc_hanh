eb50498a9a2a4139b15fc333d983fca2763f83bc
Phannvgneaog <tuandong600@gmail.com>
2023-11-24 09:22:07 +0700

    chuyen doi ngoai te

--- php/php.php
+++ php/php.php
@@ -1,17 +1,47 @@
-<?php
-  // before if statement
-  echo "THE GAME ";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Chuyển đổi ngoại tệ</title>
+</head>
+<body>
+    <h2>Chuyển đổi ngoại tệ</h2>
 
-  // if statement; condition: $_GET["response"] == "yes"
-  if ($_GET["response"] == "yes") {
-    echo "HAS BEEN QUITED";
-  }
+    <form method="post" action="">
+        <label for="amount">Số tiền:</label>
+        <input type="text" id="amount" name="amount" required><br><br>
 
-  // if statement; condition: $_GET["response"] == "no"
-  else {
-    echo "WILL BE CONTINUED IN 3 SECONDS";
-  }
+        <label for="from_currency">Từ:</label>
+        <select id="from_currency" name="from_currency">
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+            <!-- Thêm các loại tiền tệ khác nếu cần -->
+        </select><br><br>
 
-  // after if statement
-  echo "<br>AFTER IF STATEMENT";
-?>
+        <label for="to_currency">Sang:</label>
+        <select id="to_currency" name="to_currency">
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+            <!-- Thêm các loại tiền tệ khác nếu cần -->
+        </select><br><br>
+
+        <input type="submit" name="convert" value="Chuyển đổi">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["convert"])) {
+        $amount = $_POST["amount"];
+        $from_currency = $_POST["from_currency"];
+        $to_currency = $_POST["to_currency"];
+
+        // Thực hiện chuyển đổi ngoại tệ ở đây (có thể sử dụng API hoặc một tỷ giá cố định)
+        // Đây chỉ là một ví dụ đơn giản, bạn cần thay đổi để chuyển đổi thực tế
+        $conversion_rate = 1.2; // Tỷ giá từ USD sang EUR
+
+        $result = $amount * $conversion_rate;
+        echo "<br>Kết quả: " . $amount . " " . $from_currency . " = " . $result . " " . $to_currency;
+    }
+    ?>
+</body>
+</html>
