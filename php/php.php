b580fee6c38d742f84821d99ee9fd002ba5f9808
Phannvgneaog <tuandong600@gmail.com>
2023-11-28 15:41:12 +0700

    function

--- php/php.php
+++ php/php.php
@@ -1,29 +1,9 @@
 <?php
 
-$exchangeRate = array(
-  "USD" => 22,360,
-  "EUR" => 27,368,
-  "SGD" => 17,689,
-  "JPY" => 120
-);
+$items = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
 
-$amount = $_GET["amount"];
-$currency = $_GET["currency"];
-
-echo "<h3>Exchange rate to VND</h3>";
-echo "<table border='1'>
-  <tr>
-    <th>Currency</th>
-    <th>Exchange rate</th>
-  </tr>";
-foreach ($exchangeRate as $currency => $rate) {
-  echo "<tr>
-    <td>$currency</td>
-    <td>$rate</td>
-  </tr>";
+foreach ($items as $item) {
+    echo "<img src='images/$item' width='50px' height='50px' alt='$item'>";
 }
-echo "</table>";
-
-echo "<p>Số tiền $amount $currency là: <b>" . number_format($amount * $exchangeRate[$currency], 0, ',', '.') . " VND</b></p>";
 
 ?>
